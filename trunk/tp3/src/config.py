43615312d6aa3c9b2f7702651cdfcc59c6285f16
gomox.ar <gomox.ar@f4e323ce-7eeb-11dd-8f23-d9c8888c2974>
2008-11-27 20:09:00 +0000

    Actualizado con sorprendentes funcionalidades y elevada usabilidad.

--- trunk/tp3/src/config.py
+++ trunk/tp3/src/config.py
@@ -3,7 +3,17 @@
 
 GLADE_FILE = 'glade/pizza.glade'
 PROGRAM_NAME = 'Pizzas & Pizzitas 1.0'
+
 MAIN_WINDOW = 'main_window'
 ABOUT_WINDOW = 'acerca_de'
 NUEVO_PEDIDO_WINDOW = 'nuevo_pedido'
 
+DATOS_PRODUCTO = 'datos_producto'
+LISTA_PRODUCTOS = 'lista_productos'
+
+DATOS_PEDIDO_INGRESO = 'datos_pedido_ingreso'
+LISTA_INGRESO = 'lista_ingreso'
+
+
+LISTA_INSUMOS = 'lista_insumos'
+LISTA_CLIENTES = 'lista_clientes'
