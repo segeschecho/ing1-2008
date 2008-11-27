98efd3d977b358b43162a3c9d6759bcfcb96d6e1
gomox.ar <gomox.ar@f4e323ce-7eeb-11dd-8f23-d9c8888c2974>
2008-11-27 18:03:59 +0000

    Actualizada la GUI y el código.

---
+++ tp3/pysrc/prueba/config.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#! -*- encoding: utf-8 -*-
+
+GLADE_FILE = 'glade/pizza.glade'
+PROGRAM_NAME = 'Pizzas & Pizzitas 1.0'
+MAIN_WINDOW = 'main_window'
+ABOUT_WINDOW = 'acerca_de'
+NUEVO_PEDIDO_WINDOW = 'nuevo_pedido'
+
